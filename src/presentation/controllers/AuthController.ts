a5c43587aa92e9052e8626ed4686c7915b6840f4
Ahmed Gafsi <ahmedgafsi2.0@gmail.com>
2025-04-19 12:22:23 +0100

    refactoring auth to oop

---
+++ src/presentation/controllers/AuthController.ts
@@ -0,0 +1,106 @@
+import dotenv from "dotenv";
+import { Request, Response } from "express";
+import path from "path";
+import Token from "../../models/Tokens";
+import { IUser } from "../../models/User";
+import {
+    createAccessToken,
+    createRefreshToken,
+    deleteRefreshToken,
+    verifyRefreshToken
+} from "../../services/tokens.service";
+import { getUser } from "../../services/user.services";
+
+const envFilePath = path.join(__dirname, "../../.env");
+dotenv.config({ path: envFilePath });
+
+export class AuthController {
+  async login(req: Request, res: Response) {
+    const user = req.body.user;
+    try {
+      const accesstoken = createAccessToken(user);
+      const refreshtoken = await createRefreshToken(user);
+
+      res.cookie("refreshtoken", refreshtoken, {
+        httpOnly: true,
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      });
+
+      return res.status(200).json({ accesstoken, role: user.role });
+    } catch (error) {
+      res.status(500).json({ error, msg: "login failed" });
+    }
+  }
+
+  async getLoggedUser(req: Request, res: Response) {
+    try {
+      const user = await getUser(req.body.authPayload.id);
+
+      res.status(200).json({
+        id: user?.id,
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        email: user?.email,
+        role: user?.role,
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async logout(req: Request, res: Response) {
+    const cookies = req.cookies;
+    const token = cookies.refreshtoken;
+
+    if (!cookies.refreshtoken) {
+      return res.status(204).json({
+        msg: "no token provided",
+      });
+    }
+    try {
+      const decoded = await verifyRefreshToken(token);
+      const userId = decoded.id;
+      const getuser = await Token.findOne({ user: userId });
+      if (getuser) {
+        res.clearCookie("refreshtoken", {
+          httpOnly: true,
+        });
+        deleteRefreshToken(userId, token);
+        return res.status(200).json({
+          msg: "logout: success",
+        });
+      }
+      return res.status(204).send();
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async handleRefreshToken(req: Request, res: Response) {
+    const cookies = req.cookies;
+
+    if (!cookies.refreshtoken) {
+      return res.status(401).json({
+        msg: "No refreshtoken provided please checkout your input data",
+      });
+    }
+    try {
+      const decoded = await verifyRefreshToken(cookies.refreshtoken);
+      const userId = decoded.id;
+      const getuser = await getUser(userId);
+      let user = {
+        id: getuser?._id,
+        email: getuser?.email,
+        role: getuser?.role,
+        firstName: getuser?.firstName,
+        lastName: getuser?.lastName,
+        phoneNumber: getuser?.phoneNumber,
+      } as IUser;
+      const accesstoken = createAccessToken(user);
+
+      return res.status(200).json({ accesstoken });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+}
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,27 +1,21 @@
 import { Router } from "express";
 import {
-    getLoggedUser,
-    handleRefreshToken,
-    login,
-    logout,
-} from "../controllers/auth.controller";
-import {
     resetPassword,
     sendPasswordResetToken,
 } from "../controllers/forgetpassword.controller";
 import { authorize } from "../middleware/auth.middleware";
 import { verifyChangePasswordToken } from "../middleware/forgetpassword.middleware";
 import { checkCredentials } from "../middleware/user.middleware";
+import { AuthController } from "../presentation/controllers/AuthController";
 
 const router = Router();
-("");
+const authController = new AuthController();
 
-//router.post('/', login);
-router.post("/auth", [checkCredentials, login]);
-router.get("/refreshtoken", handleRefreshToken);
-router.get("/logged", authorize, getLoggedUser);
+router.post("/auth", [checkCredentials, authController.login.bind(authController)]);
+router.get("/refreshtoken", authController.handleRefreshToken.bind(authController));
+router.get("/logged", authorize, authController.getLoggedUser.bind(authController));
 router.post("/checkemail", sendPasswordResetToken);
 router.post("/forgetpassword/:token", verifyChangePasswordToken, resetPassword);
-router.get("/logout", logout);
+router.get("/logout", authController.logout.bind(authController));
 
 export default router;
